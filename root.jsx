ef19b4ab491d1a62521b97c4ce8981eac2132cf9
abduraufova_ <92726131+Madina050801@users.noreply.github.com>
2022-01-11 22:48:19 +0500

    smth

---
+++ root.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Componets/Header/index.jsx';
+import Main from '../Componets/Main/index.jsx';
+
+const Root = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Main></Main>
+        </div>
+    )
+}
+export default Root;
